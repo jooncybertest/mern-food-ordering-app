19cb39c8a50a76516d21577558611bdbf5bfe983
Junsoo Kim <155798033+jooncybertest@users.noreply.github.com>
2024-04-16 19:28:15 -0400

    complete backend setup

---
+++ backend/src/index.ts
@@ -0,0 +1,20 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+
+const app = express();
+// adds middleware to my Express application that automatically parses incoming requests with JSON payloads.
+// It allows the server to easily read JSON data sent in the body of requests, which is common in API communication.
+app.use(express.json());
+//This line enables CORS (Cross-Origin Resource Sharing) on your server. 
+//CORS is a security feature that allows or restricts requests to your server based on the origin (i.e., the domain) of the request. 
+//Without this, web applications hosted on different domains from your server wouldn't be able to interact with it.
+app.use(cors());
+
+app.get("/test", async (req: Request, res: Response) => {
+  res.json({ message: "Hello!" });
+});
+
+app.listen(3000, () => {
+  console.log("server started on localhost:3000");
+});
