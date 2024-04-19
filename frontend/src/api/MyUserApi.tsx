3c728cf7efec7a3249a27d62be83a42377695e2e
Junsoo Kim <155798033+jooncybertest@users.noreply.github.com>
2024-04-18 21:42:30 -0400

    create user request

---
+++ frontend/src/api/MyUserApi.tsx
@@ -0,0 +1,39 @@
+import { useMutation } from "react-query";
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+type CreateUserRequest = {
+  auth0Id: string;
+  email: string;
+};
+
+export const useCreateMyUser = () => {
+  const createMyUserRequest = async (user: CreateUserRequest) => {
+    const response = await fetch(`${API_BASE_URL}/api/my/user`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    });
+
+    if (!response.ok) {
+      const errorBody = await response.text();
+      throw new Error(`Failed to create user: ${errorBody}`);
+    }
+  };
+
+  const {
+    mutateAsync: createUser,
+    isLoading,
+    isError,
+    isSuccess,
+  } = useMutation(createMyUserRequest);
+
+  return {
+    createUser,
+    isLoading,
+    isError,
+    isSuccess,
+  };
+};
--- frontend/src/components/MobileNav.tsx
+++ frontend/src/components/MobileNav.tsx
@@ -34,7 +34,12 @@ export const MobileNav = () => {
           {isAuthenticated ? (
             <MobileNavLinks />
           ) : (
-            <Button className="flex-1 font-bold bg-orange-500">Log In</Button>
+            <Button
+              onClick={async () => await loginWithRedirect()}
+              className="flex-1 font-bold bg-orange-500"
+            >
+              Log In
+            </Button>
           )}
         </SheetDescription>
       </SheetContent>
