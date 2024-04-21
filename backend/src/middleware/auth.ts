91daa4ac34a053b9410297aa073198ce85884380
Junsoo Kim <155798033+jooncybertest@users.noreply.github.com>
2024-04-21 09:20:20 -0400

    update user profile api

--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -1,7 +1,52 @@
+import { NextFunction, Request, Response } from "express";
 import { auth } from "express-oauth2-jwt-bearer";
+import jwt from "jsonwebtoken";
+import User from "../models/user";
+
+declare global {
+  namespace Express {
+    interface Request {
+      userId: string;
+      auth0Id: string;
+    }
+  }
+}
 
 export const jwtCheck = auth({
   audience: process.env.AUTH0_AUDIENCE,
   issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
   tokenSigningAlg: "RS256",
 });
+
+export const jwtParse = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { authorizaiton } = req.headers;
+
+  const authString = authorizaiton as string;
+
+  if (!authString || !authString.startsWith("Bearer ")) {
+    return res.sendStatus(401);
+  }
+
+  const token = authString.split(" ")[1];
+
+  try {
+    const decoded = jwt.decode(token) as jwt.JwtPayload;
+    const auth0Id = decoded.sub;
+
+    const user = await User.findOne({ auth0Id });
+
+    if (!user) {
+      return res.sendStatus(401);
+    }
+    // add information on request
+    req.auth0Id = auth0Id as string;
+    req.userId = user._id.toString();
+    next();
+  } catch (error) {
+    return res.sendStatus(401);
+  }
+};
