c6be08bc86dfc9fcba3b719bd9900f32eb6f4e0e
Junsoo Kim <155798033+jooncybertest@users.noreply.github.com>
2024-04-21 16:57:03 -0400

    d

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,13 +7,7 @@ import Auth0ProviderWithNavigate from "./auth/Auth0ProviderWithNavigate";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { Toaster } from "sonner";
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-    },
-  },
-});
+
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
