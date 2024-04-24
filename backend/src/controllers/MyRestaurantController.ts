546af71bcf4f949f4d60d3dfc844577d3fe7f5de
Junsoo Kim <155798033+jooncybertest@users.noreply.github.com>
2024-04-24 11:29:20 -0400

    Update MyRestaurantController.ts

--- backend/src/controllers/MyRestaurantController.ts
+++ backend/src/controllers/MyRestaurantController.ts
@@ -29,7 +29,7 @@ const createMyRestaurant = async (req: Request, res: Response) => {
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Something went wrong" });
-  }
+  } 
 };
 
 export default {
