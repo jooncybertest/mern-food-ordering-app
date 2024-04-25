0a20840857652fdbad7eb29b15b8583bf8f0f642
Junsoo Kim <155798033+jooncybertest@users.noreply.github.com>
2024-04-25 13:19:40 -0400

    pre-populate restaurant form

--- frontend/src/pages/ManageRestaurantPage.tsx
+++ frontend/src/pages/ManageRestaurantPage.tsx
@@ -1,9 +1,13 @@
-import { useCreateMyRestaurant } from "@/api/MyRestaurantApi";
+import {
+  useCreateMyRestaurant,
+  useGetMyRestaurant,
+} from "@/api/MyRestaurantApi";
 import ManageRestaurantForm from "@/forms/manage-restaurant-form/ManageRestaurantForm";
 
 export default function ManageRestaurantPage() {
   const { createRestaurant, isLoading } = useCreateMyRestaurant();
+  const { restaurant } = useGetMyRestaurant();
   return (
-    <ManageRestaurantForm onSave={createRestaurant} isLoading={isLoading} />
+    <ManageRestaurantForm restaurant={restaurant} onSave={createRestaurant} isLoading={isLoading} />
   );
 }
