afe50c1ba44361c04c7cb5717bca79814b3a7cdc
Junsoo Kim <155798033+jooncybertest@users.noreply.github.com>
2024-05-02 18:07:39 -0400

    updated user profile form

--- frontend/src/components/CheckoutButton.tsx
+++ frontend/src/components/CheckoutButton.tsx
@@ -56,6 +56,8 @@ export const CheckoutButton = ({ onCheckout, disabled }: Props) => {
           currentUser={currentUser}
           onSave={onCheckout}
           isLoading={isGetUserLoading}
+          title="Confirm Delivery Details"
+          buttonText="Continue to payment"
         />
       </DialogContent>
     </Dialog>
