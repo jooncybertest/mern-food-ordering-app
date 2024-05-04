13b528e49162802872b031a209d65475b4c16709
Junsoo Kim <155798033+jooncybertest@users.noreply.github.com>
2024-05-03 22:31:25 -0400

    create order type
    create order type in backend
    Order has been separated from restaurant type
    It is efficient to be separated since lots of fetch requests will be used.
    It is for convinience.

---
+++ backend/src/models/order.ts
@@ -0,0 +1,28 @@
+import mongoose, { mongo } from "mongoose";
+
+const orderSchema = new mongoose.Schema({
+  restaurant: { type: mongoose.Schema.Types.ObjectId, ref: "Restaurant" },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  deliveryDetails: {
+    email: { type: String, requried: true },
+    name: { type: String, requried: true },
+    addressLine1: { type: String, requried: true },
+    city: { type: String, requried: true },
+  },
+  cartItems: [
+    {
+      menuItemId: { type: String, required: true },
+      quantity: { type: String, required: true },
+      name: { type: String, required: true },
+    },
+  ],
+  totalAmount: Number,
+  status: {
+    type: String,
+    enum: ["placed", "paid", "inProgress", "outForDelivery", "delivered"],
+  },
+  createdAt: { type: Date, default: Date.now },
+});
+
+const Order = mongoose.model("Order", orderSchema);
+export default Order;
