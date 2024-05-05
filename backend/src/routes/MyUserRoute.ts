2a0d6f6f731fd82a29629043e6c3803e7d8c6401
Junsoo Kim <155798033+jooncybertest@users.noreply.github.com>
2024-05-05 09:40:35 -0400

    update order api

--- backend/src/routes/MyUserRoute.ts
+++ backend/src/routes/MyUserRoute.ts
@@ -2,18 +2,27 @@ import express from "express";
 import MyUserController from "../controllers/MyUserController";
 import { jwtCheck, jwtParse } from "../middleware/auth";
 import { validateMyUserRequest } from "../middleware/validation";
+import MyRestaurantController from "../controllers/MyRestaurantController";
 
 const router = express.Router();
 
 router.get("/", jwtCheck, jwtParse, MyUserController.getCurrentUser);
-// if post request, it will get passed to the MyUserController.createCurrentUser
+
 router.post("/", jwtCheck, MyUserController.createCurrentUser);
+
 router.put(
   "/",
   jwtCheck,
   jwtParse,
   validateMyUserRequest,
   MyUserController.updateCurrentUser
+);
+
+router.patch(
+  "/order/:orderId/status",
+  jwtCheck,
+  jwtParse,
+  MyRestaurantController.updateOrderStatus
 );
 
 export default router;
